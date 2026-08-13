4996177e906fca93f34e698386662551ff47f73f
Buiscy <Aaron.regterschot@gmail.com>
2026-08-13 09:27:42 +0000

    Push

--- _printf.c
+++ _printf.c
@@ -4,3 +4,57 @@ int _printf(const char *format, ...)
 {
  int i; r
 }
+
+void print_all(const char * const format, ...)
+{
+
+	const char *p;
+	va_list args;
+	const char *s;
+	const char *separator;
+
+	separator = "";
+
+	p = format;
+
+	va_start(args, format);
+
+	while (p != NULL && *p != '\0')
+	{
+		switch (*p)
+		{
+			case 'c':
+				printf("%s%c", separator, va_arg(args, int));
+				separator = ", ";
+				break;
+
+			case 'i':
+				printf("%s%d", separator, va_arg(args, int));
+				separator = ", ";
+				break;
+
+			case 'f':
+				printf("%s%f", separator, va_arg(args, double));
+				separator = ", ";
+				break;
+
+			case 's':
+				s = va_arg(args, char *);
+
+				if (s == NULL)
+				{
+					s = "(nil)";
+				}
+
+				printf("%s%s", separator, s);
+				separator = ", ";
+				break;
+
+		}
+
+		p++;
+	}
+
+	va_end(args);
+	printf("\n");
+}
