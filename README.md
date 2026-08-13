e6770ef414d2df106d1b16ba7917a05037dac8cd
Buiscy <Aaron.regterschot@gmail.com>
2026-08-13 09:08:09 +0000

    Thrid time is the charm

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This function should print S and C using % as a specifier.
+
+This Print is complied with: gcc -Wall -Wextra -Werror -pedantic -std=gnu89 -Wno-format *.c
--- main.h
+++ main.h
@@ -1,1 +1,10 @@
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
 int _printf(const char *format, ...);
+
+#endif
