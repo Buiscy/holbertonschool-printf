58b67039a5ee64658c1b951ab2aba17698e9b107
Buiscy <Aaron.regterschot@gmail.com>
2026-08-16 22:03:36 +1000

    Work

--- Int_func.c
+++ Int_func.c
@@ -17,11 +17,17 @@ int print_int (int a)
 	else
 		tmp = (unsigned int) a;
 
-	while (tmp / 1 > 9)
+	while (tmp > 9)
 		{
-			i = temp % 10;
+			i = tmp % 10;
 			digit = i + '0';
-
+			write(1, &digit, 1)
+			tmp = tmp / 10;
+			if (tmp < 10)
+				{
+					digit = tmp + '0';
+					write(1, &digit, 1)
+				}
 		}
 }
 
